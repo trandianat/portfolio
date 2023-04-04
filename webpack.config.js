d2a749054c71cf73456e43728046c1ced33df1c4
Diana Tran <trandianat@gmail.com>
2023-04-04 15:14:08 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: path.join(__dirname, "src", "index.tsx"), // or "./src/index.tsx"
+    output: { path: path.join(__dirname, "build"), filename: "index.bundle.js", publicPath: "/" },
+    mode: process.env.NODE_ENV || "development",
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+        modules: [path.resolve(__dirname, "src"), "node_modules"]
+    },
+    devServer: { static: path.join(__dirname, "src"), historyApiFallback: true },
+    module: {
+        rules: [
+            { 
+                test: /\.(js|jsx)$/, 
+                exclude: /node_modules/, 
+                use: ["babel-loader"] 
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: ["ts-loader"],
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            // {
+            //     test: /\.svg$/,
+            //     use: ["@svgr/webpack"]
+            // },
+            // { 
+            //     test: /\.ttf$/,
+            //     type: "asset/resource"
+            // },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // favicon: "./src/assets/favicon.ico",
+            template: path.join(__dirname, "src", "index.html"),
+        }),
+    ],
+};
