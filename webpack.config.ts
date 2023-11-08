dc5de6f449af17784a2bb216909dd0e91ba0014a
Diana Tran <trandianat@gmail.com>
2023-11-07 21:34:08 -0500

    Bundled vendors by common dependencies

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,21 +2,34 @@ const path = require("path");
 const Dotenv = require("dotenv-webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+const getNodeModulesRegExp = (deps: string[]) => new RegExp(`[\\/]node_modules[\\/]${deps.join('|')}`);
+const excludeNodeModulesRegExp = (deps: string[]) => new RegExp(`[\\/]node_modules[\\/](?!(${deps.join('|')})).*`);
+
+const awsCacheGroupDeps = ['@aws-amplify', '@aws-crypto', '@aws-sdk', 'amazon-cognito-identity-js/es'];
+const reactCacheGroupDeps = ['react', 'react-dom']; 
+const tensorflowCacheGroupDeps = ['@tensorflow-models/coco-ssd', '@tensorflow/tfjs-backend-cpu', '@tensorflow/tfjs-backend-webgl'];
+const vendorCacheGroupDeps = [
+    ...awsCacheGroupDeps,
+    ...reactCacheGroupDeps,
+    ...tensorflowCacheGroupDeps,
+];
+
+const AwsCacheGroup = { name: 'aws', test: getNodeModulesRegExp(awsCacheGroupDeps) };
+const ReactCacheGroup = { name: 'react', test: getNodeModulesRegExp(reactCacheGroupDeps) };
+const TensorflowCacheGroup = { name: 'tensorflow', test: getNodeModulesRegExp(tensorflowCacheGroupDeps) };
+const VendorCacheGroup = { name: 'vendor', test: excludeNodeModulesRegExp(vendorCacheGroupDeps) };
+const cacheGroups = {
+    aws: AwsCacheGroup,
+    tensorflow: TensorflowCacheGroup,
+    react: ReactCacheGroup,
+    vendor: VendorCacheGroup,
+};
+
 module.exports = {
-    entry: {
-        index: {
-            dependOn: 'shared',
-            import: path.join(__dirname, "src", "index.tsx") // or "./src/index.tsx"
-        },
-        projects: {
-            dependOn: 'shared',
-            import: './src/pages/projects/index.tsx'
-        },
-        shared: ['@emotion/react', 'aws-amplify']
-    },
+    entry: path.join(__dirname, "src", "index.tsx"), // or "./src/index.tsx"
     output: {
         clean: true,
-        filename: "[name].bundle.js",
+        filename: "[name].[contenthash].js",
         path: path.join(__dirname, "build"),
         publicPath: "/"
     },
@@ -60,7 +73,14 @@ module.exports = {
         }),
     ],
     optimization: {
-        runtimeChunk: 'single',
-        splitChunks: { chunks: 'all' } // Splits common dependencies into chunks
+        splitChunks: {
+            chunks: 'all', // Splits common dependencies into chunks
+            cacheGroups: {
+                [cacheGroups.aws.name]: cacheGroups.aws,
+                [cacheGroups.react.name]: cacheGroups.react,
+                [cacheGroups.tensorflow.name]: cacheGroups.tensorflow,
+                [cacheGroups.vendor.name]: cacheGroups.vendor,
+              },
+        }
     },
 };
